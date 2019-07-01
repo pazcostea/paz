8b85ac4435e8c56dd0c9f277331ab6de4efca3e8
formacion <formacion@gtt.es>
2019-07-01 12:17:28 +0200

    Primeras pruebas de c#

---
+++ 01/Program.cs
@@ -0,0 +1,42 @@
+﻿/* 
+* Paz Costea 
+* 01/07/2019 
+* 09:10
+*/ 
+using System;
+
+namespace _01
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {                     
+            // Probando a convertir un string en un entero y realizar una operación
+            /*           
+            int num1 = int.Parse (args[0]);
+            int num2 =  int.Parse (args[1]);
+            int multiplicacion = num1 * num2; 
+            Console.WriteLine("El resultado es: " + '\n' + '\"' +  multiplicacion);
+             */
+
+            //Cambiar el tipo de numero de doble a entero
+            /* double x = 1234.7;
+            int a;
+            a = (int)x;
+            Console.WriteLine(a);*/
+
+            //Strings
+
+            //string cadena = "12345678910";
+            //Console.WriteLine(cadena.Substring(2,5)); 
+            
+            //string cadena = "Cadena de Texto";
+            //Console.WriteLine(cadena.IndexOf(args[0]));
+
+            string numeros ="123456789";
+            Console.WriteLine(numeros.Substring(numeros.IndexOf(args[0]),5));
+
+        }
+    }
+}
---
+++ 02bisiesto/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _02bisiesto
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Int32 annoBisiesto = Int32.Parse(args[0]);
+            if (esBisiesto(annoBisiesto))
+            {
+                Console.WriteLine("El año " + annoBisiesto + " es bisiesto");
+            }
+            else
+            {
+                Console.WriteLine("El año " + annoBisiesto + " no es bisiesto");
+            }
+        }
+        static bool esBisiesto(int bisi)
+        {
+
+           return ((bisi % 400 == 0)||  ((bisi % 4 == 0) && (bisi % 100 ==0)));          
+
+        }
+
+            /*Int32 bisiesto = Int32.Parse(args[0]);
+            if (bisiesto % 400 == 0)
+            {
+                Console.WriteLine("El año " + bisiesto + " es bisiesto");
+            }
+            else if ((bisiesto % 4 == 0) && (bisiesto % 100 ==0))
+            {
+                Console.WriteLine("El año " + bisiesto + " es bisiesto");
+            }
+            else {
+                Console.WriteLine("El año " + bisiesto + " no es bisiesto");
+            }*/
+            
+        
+    }
+}
