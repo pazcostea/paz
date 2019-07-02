c8185f62e0a6d6b6159ba4de38501a992983271f
formacion <formacion@gtt.es>
2019-07-02 08:14:34 +0200

    Cambios martes mañana

---
+++ 06ordeninverso/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+//Leer 10 datos numéricos y mostrarlos en orden inverso
+namespace _06ordeninverso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 9; i >= 0; i--)
+            {               
+                Console.WriteLine(args[i]);
+            }
+            
+        }
+    }
+}
