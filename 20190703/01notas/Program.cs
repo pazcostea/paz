ee4bdee50b4659086c4464525cb48be3e750a710
formacion <formacion@gtt.es>
2019-07-03 08:30:28 +0200

    01notas

---
+++ 20190703/01notas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+/*
+Permite al usuario introducir 10 notas (sin utilizar los argumentos) y calcula la media
+ */
+namespace _01notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numnot = 3;
+            int [] notas = new int [numnot];
+            int sumatorio = 0;
+            int media = 0;
+            for (int i = 0; i < numnot; i++)
+            {
+                Console.Write("Introduzca la nota: ");
+                notas[i] = int.Parse(Console.ReadLine());
+                sumatorio = sumatorio + notas[i];
+            }
+            media = sumatorio / numnot;
+            Console.WriteLine("La nota media es: " + media);
+        }
+    }
+}
