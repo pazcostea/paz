c4c4b71757530d6ddcd7626c6306394ea5b3fedb
formacion <formacion@gtt.es>
2019-07-03 11:34:29 +0200

    Cambios

---
+++ 20190703/03vehiculos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03vehiculos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(vehiculo.volante);
+            vehiculo coche = new vehiculo ();
+            coche.arrancar();
+            coche.setNumPuertas(10);                     
+            coche.getNumPuertas();            
+            vehiculo coche2 = new vehiculo ();
+        }
+    }
+}
---
+++ 20190703/03vehiculos/vehiculo.cs
@@ -0,0 +1,86 @@
+namespace _03vehiculos
+{
+    public class vehiculo
+    {
+        
+        private byte numRuedas;            
+        private ushort capacidad;
+        private string color;
+        //en hexadecimal #RRGGBB        
+        private byte numPuertas;
+        public static string [] lCombustible = {"Gasolina", "Diesel", "Hibrido", "Eléctrico"};         
+        private bool esAutomatico;
+
+        private int velocidadMaxima;
+	    public byte getNumRuedas() 
+        {
+		    return this.numRuedas;
+	    }
+	    public void setNumRuedas(byte numRuedas) 
+        {
+		    this.numRuedas = numRuedas;
+	    }
+        public static int volante = 1;     
+        bool encendido = false;   
+        public void arrancar ()
+        {
+            encendido = true;
+            if (encendido == true)
+            {
+                System.Console.WriteLine("Brrr");                
+            }
+            else
+            {
+                System.Console.WriteLine("Apagado");
+            }           
+                
+        }
+            public ushort getCapacidad() 
+            {
+            return this.capacidad;
+            }
+            public void setCapacidad(ushort capacidad) 
+            {
+            this.capacidad = capacidad;
+            }
+            public string getColor() 
+            {
+		    return this.color;
+	        }
+	        public void setColor(string color) 
+            {		        
+                this.color = color;
+	        }
+            public byte getNumPuertas()             
+            {		        
+                System.Console.WriteLine(numPuertas);
+                return this.numPuertas;
+	        }
+	        public void setNumPuertas(byte numPuertas) 
+            {		        
+                this.numPuertas = numPuertas;
+	        }
+            public bool getEsAutomatico()
+            {
+		        return this.esAutomatico;
+	        }
+            public void isEsAutomatico(bool esAutomatico) 
+            {
+		        this.esAutomatico = esAutomatico;
+	        }
+            public int getVelocidadMaxima() 
+            {
+		        return this.velocidadMaxima;
+	        }
+	        public void setVelocidadMaxima(int velocidadMaxima) 
+            {
+		        this.velocidadMaxima = velocidadMaxima;
+	        }
+            public vehiculo()
+        {
+            this.setColor("Rojo");
+            this.setNumRuedas(4);
+            System.Console.WriteLine("Vehículo creado" + color + numRuedas);
+        }	
+    }
+}
