610af95bdac4404b154dcfd785c34daa62427a01
formacion <formacion@gtt.es>
2019-07-08 07:56:46 +0200

    Videojuego

--- 20190705/videojuego/Controllers/ToDoController.cs
+++ 20190705/videojuego/Controllers/ToDoController.cs
@@ -18,13 +18,15 @@ namespace videojuego.Controllers
         public ToDoController()
         {
             Console.WriteLine(store.todos.Count);            
-        }
+        }       
 
+        //Método para obtener la lista de videojuegos que se han introducido
         [HttpGet]
         public List<Datosvideoj> getTodoVideo()
         {
             return this.store.todos;
         }
+        //Método para introducir videojuegos
         [HttpPost]
         public Datosvideoj PostTodo (Datosvideoj item)
         {
@@ -33,6 +35,33 @@ namespace videojuego.Controllers
             return item;
         }
 
+        //Método para obtener la información de un videojuego por id
+        [HttpGet("{Id}")]
+        public Datosvideoj getTodoItems(long Id)
+        {
+            return this.store.todos.Find(todo => todo.Id == Id);            
+        }
+        //Método para buscar en la lista de videojuegos por una cadena de texto
+         [HttpGet("buscar/{Cadena}")]
+        public List<Datosvideoj> buscar(string Cadena)
+        {            
+            return this.store.todos.FindAll(Datosvideoj => 
+            {
+                int anyo = Datosvideoj.anyolanzamiento;
+                string cadAnnyo = anyo.ToString();
+                //|| Datosvideoj.anyolanzamiento == anyo
+               return Datosvideoj.companyia.Contains(Cadena) || Datosvideoj.Titulo.Contains(Cadena) || cadAnnyo.Contains(Cadena);
+            });
+        }
+        //PUT /api/videojuegos/[id], le paso todas las propiedades que tiene el objeto para modificar la información de un Id
+        //[HttpPut("{Id}")]
 
+        
+        //DELETE /api/videojuego/[id] --> Borra
+        [HttpDelete("{Id}")]
+        public void eliminar(int Id)
+        {
+            this.store.todos.RemoveAt(Id);
+        }
     }
 }
